f7c35ccf2e688b804ae67b21a3c16666baca5888
Matcenko <pavelmatsenko@gmail.com>
2022-01-10 13:39:29 +0200

    added fonts; reseted css, small dependencies fixes.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
 import {useState} from "react";
 
 import burger from '../../assets/burger.svg';
+import logo from '../../assets/logo.svg';
+import search from '../../assets/search.svg';
+import heart from '../../assets/heart.svg';
+import cart from '../../assets/cart.svg';
 import styles from './Header.module.css';
 
 export default function Header () {
@@ -15,6 +19,7 @@ export default function Header () {
             alt="menu"/>
         {showMobileMenu &&(
           <aside className={styles.mobileMenu}>
+            <img src={logo} alt="Tentor"/>
             <p>
               Магазин
             </p>
@@ -24,9 +29,17 @@ export default function Header () {
             <p>
               Контакти
             </p>
-            <p>
-              Пошук
-            </p>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+             <img src={search} alt="search"/>
+              <p>
+                Пошук
+              </p>
+            </div>
+            <div>
+              <img src={heart} alt="liked"/>
+              <img src={cart} alt="cart"/>
+              <p>₴0.0</p>
+            </div>
           </aside>
         )}
       </header>)
