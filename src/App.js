bbc99d5fb5b018d6b0148271c18dfe6aa918adbe
hasskell <hasskell@hasskells-MacBook-Pro.local>
2022-01-14 14:45:25 +0200

    new eslint props rule

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer'
+// import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import DesktopHeader from './components/DesktopHeader/DesktopHeader'
+
 
 function App() {
   return (
     <div>
-      <Header/>
+      {/* <Header/> */}
+      <DesktopHeader/>
       <Footer/>
     </div>
   );
